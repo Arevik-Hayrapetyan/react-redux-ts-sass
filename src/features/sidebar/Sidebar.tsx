02d81953f8195ffca6b0cbe80a997b947e1997bb
Arevik Hayrapetyan <arevhayrapetyan88@gmail.com>
2022-11-25 12:45:07 +0400

    added styles

--- src/features/sidebar/Sidebar.tsx
+++ src/features/sidebar/Sidebar.tsx
@@ -1,12 +1,29 @@
-import React from 'react';
+import React from 'react'
 import './Sidebar.scss'
+import { HiClipboardList } from 'react-icons/hi'
+import { BsFillPeopleFill } from 'react-icons/bs'
+import { HiCube } from 'react-icons/hi'
 
 function Sidebar() {
   return (
-    <div className='sidebarContainer'>
-      Sidebar
+    <div className="sidebarContainer">
+      <div className="svgContainer">
+        <span className="svgItem">
+          <HiCube />
+        </span>
+      </div>
+      <div className="svgContainer">
+        <span className="svgItem">
+          <HiClipboardList />
+        </span>
+      </div>
+      <div className="svgContainer">
+        <span className="svgItem">
+          <BsFillPeopleFill />
+        </span>
+      </div>
     </div>
-  );
+  )
 }
 
-export default Sidebar;
+export default Sidebar
